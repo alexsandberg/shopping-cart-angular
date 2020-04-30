7b2bd7c4de72c7b9a090dead0e3249b2bed26cc2
Alex Sandberg <alexsandberg7@gmail.com>
2020-04-30 13:09:53 -0600

    hide or display admin options in dropdown based on whether user is admin

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { AppUser } from './models/app-user';
+import { switchMap } from 'rxjs/operators';
+import { UserService } from './user.service';
 
 @Injectable({
     providedIn: 'root'
@@ -10,7 +13,9 @@ import { ActivatedRoute } from '@angular/router';
 export class AuthService {
     user$: Observable<firebase.User>;
 
-    constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+    constructor(
+        private userService: UserService,
+        private afAuth: AngularFireAuth, private route: ActivatedRoute) {
         this.user$ = afAuth.authState;
     }
 
@@ -24,5 +29,18 @@ export class AuthService {
 
     logout() {
         this.afAuth.signOut();
+    }
+
+    get appUser$(): Observable<AppUser> {
+        return this.user$
+            .pipe(
+                switchMap(user => {
+                    if (user) {
+                        return this.userService.get(user.uid).valueChanges();
+                    }
+
+                    return of(null);
+                })
+            );
     }
 }
