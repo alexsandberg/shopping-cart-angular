1e3d5de9d26c7c81b24a8e22551a2625bfa541dd
Alex Sandberg <alexsandberg7@gmail.com>
2020-05-01 13:05:34 -0600

    Implement search functionality on products page

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/product.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-admin-products',
     templateUrl: './admin-products.component.html',
     styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-    products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+    products = [];
+    filteredProducts = [];
+    subscription: Subscription;
 
     constructor(private productService: ProductService) {
-        this.products$ = productService.getAll();
+        this.subscription = this.productService.getAll()
+            .subscribe(products => products.forEach(product => {
+                this.products.push(product.payload.val());
+            }));
+        this.filteredProducts = this.products;
+    }
+
+    filter(query: string) {
+        this.filteredProducts = (query) ?
+            this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+            this.products;
+    }
+
+    ngOnDestroy() {
+        this.subscription.unsubscribe();
     }
 
     ngOnInit(): void {
